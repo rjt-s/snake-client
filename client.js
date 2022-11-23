ba185a49e3408d69a1a74b9d6e27c7c00f610e0b
Rajat Sharma <srma.rjt@gmail.com>
2022-11-23 17:58:15 -0500

    added move up command to snake game

--- client.js
+++ client.js
@@ -8,7 +8,32 @@ const connect = function() {
   })
 
   // set data encoding
-  conn.setEncoding('utf-8');
+  conn.setEncoding('utf8');
+
+  // connection successfully started message
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  })
+
+  conn.on("connect", () => {
+    conn.write("Name: RJT");
+  })
+
+  // move up at connect
+  conn.on("connect", () => {
+    conn.write("Move: up");
+  })
+  // move one more
+  setTimeout(() => {
+    conn.on("connect", () => {
+      conn.write("Move: up");
+    })
+  },500)
+
+  // move with setInterval
+  setInterval(() => {
+    conn.write("Move: up");
+  },1000)
 
   conn.on("data", (data) => {
     // code that does something when some data is sent
