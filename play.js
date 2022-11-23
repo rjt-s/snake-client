cb1ec2625ac98f06a5b4837d1800b402d66d0855
Rajat Sharma <srma.rjt@gmail.com>
2022-11-23 18:23:02 -0500

    stdInput func added

--- play.js
+++ play.js
@@ -2,7 +2,26 @@
 const net = require('net');
 const connect = require('./client');
 
+// setup interface to handle user input from stdin
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 // establish a connection
+setupInput();
 console.log("Connecting...")
 connect();
 
